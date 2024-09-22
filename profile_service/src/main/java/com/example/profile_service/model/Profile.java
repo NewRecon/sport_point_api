2f51a2e0b1d8b62a2b367031538d48e7f3050e3a
Maxim Shabanov <99maxwolf99@gmail.com>
2024-09-22 23:55:17 +0300

    настроил eureka serve и роутинг через api gateway

--- profile_service/src/main/java/com/example/profile_service/model/Profile.java
+++ profile_service/src/main/java/com/example/profile_service/model/Profile.java
@@ -4,14 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
-@Entity
-@Data
 @Table(name = "profiles")
+@Entity
+@Setter
+@Getter
+@ToString
 public class Profile {
 
     @Id
@@ -26,4 +31,17 @@ public class Profile {
     private Date birthday;
     private String aboutInformation;
     private String sportDisciplineId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(profileId, profile.profileId) && Objects.equals(userId, profile.userId) && Objects.equals(firstName, profile.firstName) && Objects.equals(lastName, profile.lastName) && Objects.equals(email, profile.email) && Objects.equals(phone, profile.phone) && Objects.equals(age, profile.age) && Objects.equals(birthday, profile.birthday) && Objects.equals(aboutInformation, profile.aboutInformation) && Objects.equals(sportDisciplineId, profile.sportDisciplineId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(profileId, userId, firstName, lastName, email, phone, age, birthday, aboutInformation, sportDisciplineId);
+    }
 }
